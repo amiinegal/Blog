9a90badc9a0355b50512f26c8ee252e227e48841
amiinegal <you@example.com>
2019-04-30 15:57:32 +0300

    add configuration

---
+++ config.py
@@ -0,0 +1,37 @@
+import os
+
+
+class Config:
+    BASE_QUOTES_API_URL = 'http://quotes.stormconsultancy.co.uk/random.json'
+    SECRET_KEY = '5791628bb0b13ce0c676dfde280ba245'
+
+    # SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+    # SQLALCHEMY_TRACK_MODIFICATIONS = False
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get('EMAIL_USER')
+    MAIL_PASSWORD = os.environ.get('EMAIL_PASS')
+    UPLOADED_PHOTOS_DEST ='app/static/photos'
+
+
+
+
+class ProdConfig(Config):
+    pass
+
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+
+class TestConfig(Config):
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://amin:999@localhost/blogs'
+
+class DevConfig(Config):
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://amin:999@localhost/blogs'
+    DEBUG = True
+
+config_options = {
+'development':DevConfig,
+'production':ProdConfig,
+'test':TestConfig
+
+}
